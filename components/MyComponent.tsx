19500674dd73ddb25d052bfdfd6f8ddee83d0b0d
nghiepit <me@nghiepit.dev>
2022-03-24 13:56:25 +0700

    Add Suspense bug

---
+++ components/MyComponent.tsx
@@ -0,0 +1,5 @@
+const MyComponent = () => {
+  return <div>My Component</div>;
+};
+
+export default MyComponent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import {lazy, Suspense} from 'react';
 import type {NextPage} from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+
+const MyComponent = lazy(() => import('../components/MyComponent'));
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +16,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Suspense fallback='Loading...'>
+          <MyComponent />
+        </Suspense>
         <h1 className={styles.title}>
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
